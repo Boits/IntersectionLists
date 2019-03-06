df7f31451dc6650bda192ac69c9e9ebd8f47b43a
Boits <boitsovanastya9600@gmail.com>
2019-03-06 12:26:40 +0300

    Add set

--- src/Solution.java
+++ src/Solution.java
@@ -1,17 +1,35 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class Solution {
     public static void main(String[] args) {
+//        intersectionLists();
+        intersectionSets();
+    }
 
-        List<String> list1 = new ArrayList<>(Arrays.asList("A", "B", "C", "L"));
-        List<String> list2 = new ArrayList<>(Arrays.asList("D", "A", "C", "E", "K", "B"));
+    private static void intersectionSets() {
+        Set<String> set1 = new HashSet<>(Arrays.asList("A", "B", "C", "L", "C"));
+        Set<String> set2 = new HashSet<>(Arrays.asList("D", "A", "C", "E", "K", "B"));
+
+        Set<String> set3 = new HashSet<>();
+        for (String s : set2) {
+            if (set1.contains(s)) {
+                set3.add(s);
+            }
+        }
+
+        for (String s : set3) {
+            System.out.print(s + " ");
+        }
+    }
+
+    private static void intersectionLists() {
+        List<String> list1 = Arrays.asList("A", "B", "C", "L");
+        List<String> list2 = Arrays.asList("D", "A", "C", "E", "K", "B");
 
         List<String> list3 = new ArrayList<>();
 
         for (int i = 0; i < list2.size(); i++) {
-            if(list1.contains(list2.get(i))){
+            if (list1.contains(list2.get(i))) {
                 list3.add(list2.get(i));
             }
         }
@@ -20,4 +38,5 @@ public class Solution {
             System.out.print(list3.get(i) + " ");
         }
     }
+    
 }
